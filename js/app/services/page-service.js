bfbb45aa39fff931a7a695d8e4bb337af154d5e1
Nikolay Belikov <nikmail88@gmail.com>
2015-06-05 13:17:22 +0300

    мелкая правка

--- js/app/services/page-service.js
+++ js/app/services/page-service.js
@@ -8,7 +8,7 @@
             // задаем вопрос к: 1, 3, 5
             this.getDeclOfNum = function (number, titles)
             {
-                cases = [2, 0, 1, 1, 1, 2];
+                var cases = [2, 0, 1, 1, 1, 2];
                 return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
             };
 
