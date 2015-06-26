e378d1ab2f78c7aafde46510c3aa889dece4a952
Nikolay Belikov <nikmail88@gmail.com>
2015-06-26 10:48:51 +0300

    удалить все данные

--- app/js/app/controllers/settings-controller.js
+++ app/js/app/controllers/settings-controller.js
@@ -2,10 +2,26 @@
 
 (function () {
     angular.module('eve')
-        .controller('SettingsCtrl', ['$scope', 'settingsService', function ($scope, settingsService) {
+        .controller('SettingsCtrl', ['$scope', 'settingsService', 'projectsService', function ($scope, settingsService, projectsService) {
 
             $scope.setTheme = function (theme) {
                 settingsService.setTheme(theme);
             };
+
+            $scope.removeAllData = function () {
+
+                // настройки
+                localStorage.removeItem('settings');
+
+                // проекты
+                projectsService.getProjects().then(function (projects) {
+                    projects.map(function (project) {
+                        localStorage.removeItem(project.id)
+                    });
+                });
+                localStorage.removeItem('project-list');
+
+                location.reload();
+            }
         }]);
 })();
