37dd407f1c307122ce7cda11857ff02e0650c14c
Nikolay Belikov <nikmail88@gmail.com>
2015-06-26 09:17:49 +0300

    кнопки переключения тем

--- app/js/app/controllers/settings-controller.js
+++ app/js/app/controllers/settings-controller.js
@@ -3,8 +3,9 @@
 (function () {
     angular.module('eve')
         .controller('SettingsCtrl', ['$scope', 'settingsService', function ($scope, settingsService) {
+
             $scope.setTheme = function (theme) {
                 settingsService.setTheme(theme);
-            }
+            };
         }]);
 })();
